c18f4a0109c43b8b9348cafab9240fd46e7f0337
Pato <pato24193@gmail.com>
2023-12-03 17:36:59 +0700

    pato add firstly

---
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.codegym.controller;
+
+import com.codegym.dto.UserDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = {"/product", ""})
+public class ProductController {
+
+    @GetMapping(value = "")
+    public String goList() {
+        return "list_products";
+    }
+
+    @GetMapping(value = "/login")
+    public String goLogin(Model model) {
+        model.addAttribute("userDto", new UserDto());
+
+        return "go_login";
+    }
+
+    @PostMapping(value = "/login")
+    public String login(@ModelAttribute @Validated UserDto userDto,
+                        BindingResult bindingResult,
+                        Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "go_login";
+        }
+
+        model.addAttribute("msg", "Login success!");
+
+        return "go_login";
+    }
+}
---
+++ src/test/java/com/codegym/ProductControllerTest.java
@@ -0,0 +1,89 @@
+package com.codegym;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class ProductControllerTest {
+
+    private static WebDriver browser;
+
+    @BeforeAll
+    public static void initBrowser() {
+        System.setProperty("webdriver.chrome.driver",
+                "src/test/java/com/codegym/chromedriver/_119/chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless");
+        options.addArguments("--remote-allow-origins=*");
+        browser = new ChromeDriver(options);
+        browser.manage().window().maximize();
+        browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    public static void destroyBrowser() {
+        browser.quit();
+    }
+
+    private void slow() {
+        try {
+            Thread.sleep(2 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_Username_Empty() {
+        browser.get("http://localhost:8080/login");
+        browser.findElement(By.name("password")).sendKeys("12345678");
+        browser.findElement(By.id("btnLogin")).click();
+        slow();
+        String error = browser.findElements(By.className("text-danger")).get(0).getText();
+        Assert.hasText(error, "must not be empty");
+    }
+
+    @Test
+    public void test_Password_Empty() {
+        browser.get("http://localhost:8080/login");
+        browser.findElement(By.name("username")).sendKeys("tien.nguyen@codegym.vn");
+        browser.findElement(By.id("btnLogin")).click();
+        slow();
+        String error = browser.findElements(By.className("text-danger")).get(1).getText();
+        Assert.hasText(error, "must not be empty");
+    }
+
+    @Test
+    public void test_All_Empty() {
+        browser.get("http://localhost:8080/login");
+        browser.findElement(By.id("btnLogin")).click();
+        slow();
+        List<WebElement> textDangers = browser.findElements(By.className("text-danger"));
+        Assert.hasText(textDangers.get(0).getText(), "must not be empty");
+        Assert.hasText(textDangers.get(1).getText(), "must not be empty");
+    }
+
+    @Test
+    public void test_All_Not_Empty() {
+        browser.get("http://localhost:8080/login");
+        browser.findElement(By.name("username")).sendKeys("tien.nguyen@codegym.vn");
+        browser.findElement(By.name("password")).sendKeys("12345678");
+        browser.findElement(By.id("btnLogin")).click();
+        slow();
+        String msg = browser.findElement(By.name("text-success")).getText();
+        Assert.hasText(msg, "Login success!");
+    }
+
+}
